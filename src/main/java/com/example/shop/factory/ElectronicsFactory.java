31a87ce5c2ef491f010042bbc87132bb54e1d068
Gabrielė Každailytė <gabriele.kazdailyte@mif.stud.vu.lt>
2026-04-08 15:40:22 +0300

    Factory Method design pattern: Electronics class and ElectronicsFactory created

--- src/main/java/com/example/shop/factory/ElectronicsFactory.java
+++ src/main/java/com/example/shop/factory/ElectronicsFactory.java
@@ -1,4 +1,11 @@
 package com.example.shop.factory;
 
+import com.example.shop.model.Electronics;
+import com.example.shop.model.Product;
+
 public class ElectronicsFactory {
+
+    public Product createProduct(String name, double price) {
+        return new Electronics(name, price);
+    }
 }
--- src/main/java/com/example/shop/model/Electronics.java
+++ src/main/java/com/example/shop/model/Electronics.java
@@ -1,4 +1,27 @@
 package com.example.shop.model;
 
-public class Electronics {
+public class Electronics implements Product {
+    private String name;
+    private double price;
+
+    public Electronics(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getCategory() {
+        return "Electronics";
+    }
+
 }
