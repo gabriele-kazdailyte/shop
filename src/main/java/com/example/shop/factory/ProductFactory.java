9ce8c2a9e3dd79151b0bee90f0b69d0ca2621dfc
Gabrielė Každailytė <gabriele.kazdailyte@mif.stud.vu.lt>
2026-04-08 14:48:16 +0300

    Factory Method design pattern: ProductFactory class created

--- src/main/java/com/example/shop/factory/ProductFactory.java
+++ src/main/java/com/example/shop/factory/ProductFactory.java
@@ -1,4 +1,13 @@
 package com.example.shop.factory;
 
-public class ProductFactory {
+import com.example.shop.model.Product;
+
+public abstract class ProductFactory {
+    public abstract Product createProduct(String name, double price);
+
+    public void announceProduct(Product product) {
+        System.out.println("New product added: " + product.getName() +
+                " | Category: " + product.getCategory() +
+                " | Price: " + product.getPrice());
+    }
 }
