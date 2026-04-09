4db00cff97ea5ea59427a55a7055e7210d478ecf
Gabrielė Každailytė <gabriele.kazdailyte@mif.stud.vu.lt>
2026-04-09 23:45:48 +0300

    Store front created

--- src/main/java/com/example/shop/Controller.java
+++ src/main/java/com/example/shop/Controller.java
@@ -14,8 +14,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+
 
     private Inventory inventory = Inventory.getInstance();
     private UIObserver observer = new UIObserver();
@@ -27,31 +26,4 @@ public class Controller {
         inventory.registerObserver(new AdminLogger());
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        Product product = ProductFactory.createProduct(ProductCategory.BOOK, "Book1", 9.99, "Author1");
-        inventory.addProduct(product);
-
-        System.out.println("Notifications sent: " + inventory.getObserverCount());
-
-        OrderService basicOrder = new BasicOrder(product);
-        System.out.println("Basic order:");
-        System.out.println("Description: " + basicOrder.getDescription());
-        System.out.println("Cost: " + basicOrder.getCost());
-
-        OrderService giftOrder = new GiftWrapping(basicOrder);
-        System.out.println("\nOrder with gift wrapping:");
-        System.out.println("Description: " + giftOrder.getDescription());
-        System.out.println("Cost: " + giftOrder.getCost());
-
-        OrderService expressOrder = new ExpressDelivery(basicOrder);
-        System.out.println("\nOrder with express delivery:");
-        System.out.println("Description: " + expressOrder.getDescription());
-        System.out.println("Cost: " + expressOrder.getCost());
-
-        OrderService fullOrder = new ExpressDelivery(new GiftWrapping(new BasicOrder(product)));
-        System.out.println("\nOrder with gift wrapping and express delivery:");
-        System.out.println("Description: " + fullOrder.getDescription());
-        System.out.println("Cost: " + fullOrder.getCost());
-    }
 }
