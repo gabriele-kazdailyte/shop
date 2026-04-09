1518a6750acf6beacc3bc1b3da850c7b34def64a
Gabrielė Každailytė <gabriele.kazdailyte@mif.stud.vu.lt>
2026-04-09 11:02:11 +0300

    Observer design pattern: temporary version of UIObserver class created (will be updated when UI is fixed)

---
+++ src/main/java/com/example/shop/observer/UIObserver.java
@@ -0,0 +1,10 @@
+package com.example.shop.observer;
+
+import com.example.shop.model.Product;
+
+public class UIObserver implements ProductObserver {
+    @Override
+    public void update(Product product) {
+        System.out.println("Product: " + product + " has been updated");
+    }
+}
